89307f7a5c5465230603963c56b8d9d080eb2971
Toprak Ekin Yüksel <ekinyuksel12@gmail.com>
2025-12-15 22:43:40 +0300

    Change to std::source_location

--- sapling.h
+++ sapling.h
@@ -1,15 +1,5 @@
 #include <iostream>
-
-constexpr const char* file_name(const char* path) {
-    const char* file = path;
-    while (*path) {
-        if (*path == '/' || *path == '\\') {
-            file = path + 1;
-        }
-        path++;
-    }
-    return file;
-}
+#include <source_location>
 
 enum LogLevel
 {
@@ -26,20 +16,10 @@ private:
     
 public:
     sapling();
-    void log(LogLevel level, const char* file, int line, const std::string &message);
+    void log(LogLevel level, const std::string &message, const std::source_location location = std::source_location::current());
 };
 
-sapling::sapling()
+void sapling::log(LogLevel level, const std::string &message, const std::source_location location)
 {
-    std::cout << "sapling constructor called" << std::endl;
+    printf("[%s:%d] [%s] %s\n", location.file_name(), location.line(), LogLevelNames[level], message.c_str());
 }
-
-void sapling::log(LogLevel level, const char* file, int line, const std::string &message)
-{
-    printf("[%s:%d] [%s] %s\n", file, line, LogLevelNames[level], message.c_str());
-}
-
-#define logInfo(message) log(INFO, file_name(__FILE__), __LINE__, message)
-#define logDebug(message) log(DEBUG, file_name(__FILE__), __LINE__, message)
-#define logWarning(message) log(WARNING, file_name(__FILE__), __LINE__, message)
-#define logError(message) log(ERROR, file_name(__FILE__), __LINE__, message)
