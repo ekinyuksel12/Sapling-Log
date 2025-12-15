577f95b17a9aa5f10a9c08385153c5fca5f99d02
Toprak Ekin Yüksel <ekinyuksel12@gmail.com>
2025-12-15 23:46:08 +0300

    Add formatter function

--- sapling.cpp
+++ sapling.cpp
@@ -1,9 +1,19 @@
 #include "sapling.h"
+#include <format>
 
 void Sapling::log(LogLevel level, const std::string &message,
-    const std::source_location location)
-{
-    printf("[%s:%d] %s[%s] %s \033[0m\n", get_filename(location.file_name()),
-        location.line(), LogLevelColors[level], LogLevelNames[level],
-        message.c_str());
+    const std::source_location location) {
+        printf("%s\n", formatLog(level, message, location).c_str());
+}
+
+// Example: [main.cpp:6] [INFO] This is an informational message.
+std::string Sapling::formatLog(LogLevel level, const std::string &message,
+    const std::source_location location) {
+
+    return std::format("[{}:{}] {}[{}] {} \033[0m", 
+        get_filename(location.file_name()),
+        location.line(),
+        LogLevelColors[level],
+        LogLevelNames[level],
+        message);
 }
--- sapling.h
+++ sapling.h
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <source_location>
+#include <string>
 
-enum LogLevel
-{
+enum LogLevel{
     DEBUG,
     INFO,
     WARNING,
@@ -14,19 +14,22 @@ static const char* LogLevelColors[] = { "\033[0;37m", "\033[0;32m", "\033[0;33m"
 
 class Sapling {
 private:
-    
+    // Helper function to extract filename from path
+    constexpr const char* get_filename(const char* path) {
+        const char* file_name = path;
+        while (*path) {
+            if (*path == '/' || *path == '\\') {
+                file_name = path + 1;
+            }
+            path++;
+        }
+        return file_name;
+    }
+
+    std::string formatLog(LogLevel level, const std::string &message,
+        const std::source_location location = std::source_location::current());
+        
 public:
     void log(LogLevel level, const std::string &message,
         const std::source_location location = std::source_location::current());
-
-    constexpr const char* get_filename(const char* path) {
-    const char* file = path;
-    while (*path) {
-        if (*path == '/' || *path == '\\') {
-            file = path + 1;
-        }
-        path++;
-    }
-    return file;
-}
 };
